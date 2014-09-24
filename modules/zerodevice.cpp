81f1809b2a0f74860d877d584f6b3811ec237c39
Chr0nos <hisoka2501@free.fr>
2014-09-24 14:12:16 +0000

    fixed all last know bugs in ZeroDevice class in read mode

--- modules/zerodevice.cpp
+++ modules/zerodevice.cpp
@@ -13,7 +13,7 @@ ZeroDevice::ZeroDevice(AudioFormat* format,QObject *parent) :
     bytesCountPs = format->getBitrate();
     lastReadTime = 0;
     timer = new QTimer(this);
-    timer->setInterval(100);
+    timer->setInterval(10);
     connect(timer,SIGNAL(timeout()),this,SIGNAL(readyRead()));
 }
 qint64 ZeroDevice::writeData(const char *data, qint64 len) {
@@ -24,16 +24,22 @@ qint64 ZeroDevice::readData(char *data, qint64 maxlen) {
     const int currentTime = QTime::currentTime().msec();
     const int elapsedTime = currentTime - lastReadTime;
     int bytesToRead = format->getBytesSizeForDuration(elapsedTime);
-    if (bytesToRead > maxlen) bytesToRead = maxlen;
-    else if (bytesToRead < 0) return bytesToRead;
-    memset(data,0,bytesToRead);
 
     lastReadTime = currentTime;
+    if (bytesToRead < 0) return -1;
+    else if (!elapsedTime) return 0;
+    else if (!bytesToRead) return 0;
+    else if (bytesToRead > maxlen) bytesToRead = maxlen;
+    //qDebug() << "time: " << elapsedTime << " bytes: " << bytesToRead;
+
+    memset(data,0,bytesToRead);
+
     return bytesToRead;
 }
 bool ZeroDevice::open(OpenMode mode) {
     QIODevice::open(mode);
     if ((mode == QIODevice::ReadOnly) || (mode == QIODevice::ReadWrite)) {
+        lastReadTime = QTime::currentTime().msec();
         emit(readyRead());
         timer->start();
     }
