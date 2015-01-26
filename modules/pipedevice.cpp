c5903a29e8ce8ede8541b9660f1eb9c415c5b31f
Chr0nos <hisoka2501@free.fr>
2015-01-26 21:46:08 +0100

    changed the pipe module working method, now the recorder is on a separate thread runing an infinite loop (with low cpu usage) and a circularbuffer, it's also the first module using QThread "correctly" , the last implementation (subclassing) was wrong, i'l update soon

--- modules/pipedevice.cpp
+++ modules/pipedevice.cpp
@@ -1,14 +1,19 @@
 #include "pipedevice.h"
+#include <stdio.h>
+
 #include <QDebug>
+
+//this class is herited by QIODevice
+//it require "console" in QT arguments
+
 PipeDevice::PipeDevice(const QString name, QObject *parent) :
     QIODevice(parent)
 {
     this->setObjectName(name);
     this->hexOutput = false;
     file = NULL;
-    timer = new QTimer(this);
-    timer->setInterval(100);
-    connect(timer,SIGNAL(timeout()),this,SIGNAL(readyRead()));
+    buffer = NULL;
+    thread = NULL;
 }
 PipeDevice::~PipeDevice() {
     say("closing device");
@@ -17,23 +22,39 @@ PipeDevice::~PipeDevice() {
         delete(file);
     }
     stop();
-    delete(timer);
 }
 bool PipeDevice::open(OpenMode mode) {
+    if (isOpen()) return true;
     say("opening device");
     file = new QFile(this);
     connect(file,SIGNAL(aboutToClose()),this,SLOT(stop()));
     bool state;
     switch (mode) {
         case QIODevice::ReadOnly:
-            connect(file,SIGNAL(readyRead()),this,SIGNAL(readyRead()));
-            state = file->open(stdin,mode);
-            if (state) {
-                say("opened stdin successfully");
-                timer->start();
-                QIODevice::open(mode);
-                return state;
-            }
+            say("creating buffer 2Mb");
+            this->buffer = new CircularBuffer(2097152,this->objectName(),this);
+            connect(buffer,SIGNAL(debug(QString)),this,SLOT(say(QString)));
+            buffer->setOverflowPolicy(CircularBuffer::Drop);
+            buffer->setPacketSize(1024);
+            connect(buffer,SIGNAL(readyRead(int)),this,SIGNAL(readyRead()));
+
+            say("creating read class");
+            input = new PipeDeviceRead(buffer,1024,0);
+            connect(input,SIGNAL(debug(QString)),this,SIGNAL(debug(QString)));
+
+            say("creating new thread");
+            thread = new QThread(this);
+
+            say("moving record class to separate thread");
+            input->moveToThread(thread);
+            connect(thread,SIGNAL(started()),input,SLOT(start()));
+            connect(thread,SIGNAL(finished()),this,SLOT(stop()));
+            connect(input,SIGNAL(failed()),this,SLOT(stop()));
+
+            say("starting thread");
+            thread->start();
+
+            return QIODevice::open(mode);
             break;
         case QIODevice::WriteOnly:
             state = file->open(stdout,mode);
@@ -56,8 +77,20 @@ void PipeDevice::say(const QString message) {
     emit(debug("PipeDevice: " + message));
 }
 qint64 PipeDevice::readData(char *data, qint64 maxlen) {
-    if (!file) return -1;
-    return file->read(data,maxlen);
+    if (!input) {
+        say("read requested but not input available: did you open in ReadOnly mode ?");
+        return -1;
+    }
+    const qint64 available = bytesAvailable();
+    if (maxlen > available) maxlen = available;
+    if (!maxlen) return 0;
+
+    QByteArray sound = buffer->getCurrentPosData(maxlen);
+    //qDebug() << sound.toHex();
+
+    //i copy the buffer into the char*
+    memcpy(data,sound.data(),sound.size());
+    return sound.size();
 }
 
 qint64 PipeDevice::writeData(const char *data, qint64 len) {
@@ -70,11 +103,75 @@ qint64 PipeDevice::writeData(const char *data, qint64 len) {
     return file->write(data,len);
 }
 void PipeDevice::stop() {
-    timer->disconnect();
+    if (thread) {
+        thread->wait(3000);
+        thread->terminate();
+        thread->disconnect();
+        delete(thread);
+        thread = NULL;
+    }
+
+    if (buffer) {
+        buffer->disconnect();
+        delete(buffer);
+        buffer = NULL;
+    }
+    this->close();
     say("stoped");
 }
 void PipeDevice::setHexOutputEnabled(const bool mode) {
     this->hexOutput = mode;
     if (mode) say("switching to hexadecimal debug output");
     else say("disabling hexadecimal mode");
+}
+
+qint64 PipeDevice::bytesAvailable() {
+    if ((!input) || (!buffer)) return -1;
+    return buffer->getAvailableBytesCount() + QIODevice::bytesAvailable();
+}
+
+
+//over this line the class is PipeDeviceRead: herited of QThread
+
+PipeDeviceRead::PipeDeviceRead(CircularBuffer *buffer, const int blocksize, QObject *parent) {
+    this->setParent(parent);
+    this->buffer = buffer;
+    connect(buffer,SIGNAL(debug(QString)),this,SLOT(say(QString)));
+    this->block = blocksize;
+    file = new QFile(this);
+}
+
+PipeDeviceRead::~PipeDeviceRead() {
+    if (file->isOpen()) file->close();
+    file->disconnect();
+    delete(file);
+}
+
+void PipeDeviceRead::start() {
+    say("opening stdin");
+    if (!file->open(stdin,QIODevice::ReadOnly)) {
+        say("error: unable to open stdin");
+        return;
+    }
+
+    say("allocating memory");
+    char raw[block];
+
+    say("starting read");
+    qint64 bytesRead = 0;
+    do {
+        //filling small buffer with readed data
+        bytesRead = file->read(raw,block);
+        //taking care about read error.
+        if (bytesRead < 0) say("error: failed to read data from stdin");
+    //adding data to the circularBuffer
+    } while ((bytesRead >= 0) && (buffer->append(raw,bytesRead)));
+
+
+    say("error. bytesread: " + QString::number(bytesRead));
+    file->close();
+    emit(failed());
+}
+void PipeDeviceRead::say(const QString message) {
+    emit(debug("PipeDeviceRead: " + message));
 }
--- modules/pipedevice.h
+++ modules/pipedevice.h
@@ -4,7 +4,27 @@
 #include <QIODevice>
 #include <QString>
 #include <QFile>
-#include <QTimer>
+#include <QThread>
+#include "circularbuffer.h"
+
+
+class PipeDeviceRead : public QObject {
+    Q_OBJECT
+    public:
+    explicit PipeDeviceRead(CircularBuffer* buffer,const int blocksize,QObject* parent);
+    ~PipeDeviceRead();
+private:
+    CircularBuffer *buffer;
+    int block;
+    QFile *file;
+public slots:
+    void start();
+private slots:
+    void say(const QString message);
+signals:
+    void debug(const QString message);
+    void failed();
+};
 
 class PipeDevice : public QIODevice
 {
@@ -13,19 +33,23 @@ public:
     explicit PipeDevice(const QString name, QObject *parent = 0);
     ~PipeDevice();
     bool open(OpenMode mode);
-    void say(const QString message);
     void setHexOutputEnabled(const bool mode);
+    qint64 bytesAvailable();
 private:
     QFile *file;
-    QTimer *timer;
     qint64 readData(char *data, qint64 maxlen);
     qint64 writeData(const char *data, qint64 len);
     bool hexOutput;
+    PipeDeviceRead* input;
+    CircularBuffer *buffer;
+    QThread *thread;
 
 signals:
     void debug(const QString message);
 public slots:
     void stop();
+private slots:
+    void say(const QString message);
 
 };
 
