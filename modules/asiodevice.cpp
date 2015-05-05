da79a7a478e8b283166f7e6af2d8470cfc92c1b4
Chr0nos <hisoka2501@free.fr>
2015-05-05 23:54:15 +0200

    i'm starting the job for ASIO capabilites, so i just put thoses two files here to dont break the released project i'l do asap, but if you read this and have any knowledge in ASIO please contact me :)

--- modules/asiodevice.cpp
+++ modules/asiodevice.cpp
@@ -1,7 +1,27 @@
 #include "asiodevice.h"
+#include <boost/asio.hpp>
 
 AsioDevice::AsioDevice()
 {
 
 }
 
+qint64 AsioDevice::readData(char *data, qint64 maxlen)
+{
+    (void) data;
+    (void) maxlen;
+    return 0;
+}
+
+qint64 AsioDevice::writeData(const char *data, qint64 len)
+{
+    (void) data;
+    (void) len;
+    return 0;
+}
+
+bool AsioDevice::open(OpenMode mode)
+{
+    (void) mode;
+    return true;
+}
--- modules/asiodevice.h
+++ modules/asiodevice.h
@@ -2,11 +2,20 @@
 #define ASIODEVICE_H
 
 #include <QObject>
+#include <QIODevice>
+#include <QAbstractEventDispatcher>
+
+#include <boost/asio/io_service.hpp>
 
 class AsioDevice : public QIODevice
 {
+private:
+    boost::asio::io_service io_service;
 public:
     AsioDevice();
+    bool open(OpenMode mode);
+    qint64 readData(char *data, qint64 maxlen);
+    qint64 writeData(const char *data, qint64 len);
 };
 
 #endif // ASIODEVICE_H
