cbd26602f6fd35bdfc86c32bc4a1258499ae99a4
Chr0nos <hisoka2501@free.fr>
2015-05-20 13:17:33 +0200

    make the code more readable, added a check for setFormat

--- audioformat.cpp
+++ audioformat.cpp
@@ -2,55 +2,86 @@
 
 #include <QString>
 
-AudioFormat::AudioFormat() {
+AudioFormat::AudioFormat()
+{
     this->codec = "audio/pcm";
 }
-void AudioFormat::setChannelCount(const int channels) {
+
+void AudioFormat::setChannelCount(const int channels)
+{
     this->channelsCount = channels;
 }
-void AudioFormat::setSampleRate(const int sampleRate) {
+
+void AudioFormat::setSampleRate(const int sampleRate)
+{
     this->sampleRate = sampleRate;
 }
-void AudioFormat::setSampleSize(const int sampleSize) {
+
+void AudioFormat::setSampleSize(const int sampleSize)
+{
     this->sampleSize = sampleSize;
 }
-int AudioFormat::getChannelsCount() {
+
+int AudioFormat::getChannelsCount()
+{
     return channelsCount;
 }
-int AudioFormat::getSampleRate() {
+
+int AudioFormat::getSampleRate()
+{
     return sampleRate;
 }
-int AudioFormat::getSampleSize() {
+
+int AudioFormat::getSampleSize()
+{
     return sampleSize;
 }
-quint64 AudioFormat::getBitrate() {
+
+quint64 AudioFormat::getBitrate()
+{
     return sampleRate * sampleSize * channelsCount / 8;
 }
-void AudioFormat::setCodec(const QString codec) {
+
+void AudioFormat::setCodec(const QString codec)
+{
     this->codec = codec;
 }
-bool AudioFormat::isValid() {
+
+bool AudioFormat::isValid()
+{
     return true;
 }
-QString AudioFormat::getCodec() {
+
+QString AudioFormat::getCodec()
+{
     return codec;
 }
-quint64 AudioFormat::getBytesSizeForDuration(const int msecs) {
+
+quint64 AudioFormat::getBytesSizeForDuration(const int msecs)
+{
     if (msecs <= 0) return 0;
     return this->getBitrate() / 1000 * msecs;
 }
-const QString AudioFormat::getFormatTextInfo() {
+
+const QString AudioFormat::getFormatTextInfo()
+{
     return QString("samplesize:" + QString::number(sampleSize) + " samplerate:" + QString::number(sampleRate) + " channels:" + QString::number(channelsCount));
 }
-bool AudioFormat::setFormat(AudioFormat *format) {
+
+bool AudioFormat::setFormat(AudioFormat *format)
+{
+    if (!format) return false;
     sampleRate = format->getSampleRate();
     sampleSize = format->getSampleSize();
     codec = format->getCodec();
     channelsCount = format->getChannelsCount();
     return true;
 }
-QList<int> AudioFormat::getCommonSamplesRates() {
+
+QList<int> AudioFormat::getCommonSamplesRates()
+{
     QList<int> rates;
+
     rates << 8000 << 11025 << 16000 << 22050 << 32000 << 44100 << 48000 << 96000 << 88200 << 176400 << 192000 << 384000;
     return rates;
 }
