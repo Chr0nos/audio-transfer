31fe468ef90de8ecc43509600e632ed7bb1a15c2
Chr0nos <hisoka2501@free.fr>
2015-07-08 14:50:35 +0000

    prepared the job to handle WAVE headers in the file mode

--- modules/filedevice.cpp
+++ modules/filedevice.cpp
@@ -2,10 +2,11 @@
 #include "manager.h"
 #include <QFile>
 
-FileDevice::FileDevice(QString filePath, QObject *parent) :
+FileDevice::FileDevice(QString filePath, AudioFormat *format, QObject *parent) :
     ModuleDevice(parent)
 {
     this->file = new QFile(filePath, parent);
+    this->format = format;
 }
 
 FileDevice::~FileDevice()
@@ -35,8 +36,7 @@ ModuleDevice* FileDevice::factory(QString name, AudioFormat *format, void *userD
     FileDevice          *dev;
 
     config = (Manager::userConfig*) userData;
-    dev = new FileDevice(*config->file.filePath, parent);
-    (void) format;
+    dev = new FileDevice(*config->file.filePath,format, parent);
     (void) name;
     return dev;
 }
@@ -54,11 +54,45 @@ qint64 FileDevice::readData(char *data, qint64 maxlen)
 
 qint64 FileDevice::writeData(const char *data, qint64 len)
 {
+    quint64 wrote;
+
     if (!this->file->isWritable()) return -1;
-    return this->file->write(data, len);
+    write = this->file->write(data, len);
+    //this->writeWaveHeader();
+    return wrote;
 }
 void FileDevice::say(const QString message)
 {
     (void) message;
     //emit(debug("FileDevice: " + message));
+}
+
+void FileDevice::writeWaveHeader()
+{
+    /*
+    ** this method write the current header in the file
+    ** i used the tutorial at url:
+    ** http://mathmatrix.narod.ru/Wavefmt.html
+    */
+    qint64      pos;
+    QByteArray  header;
+
+    pos = this->file->pos();
+    header.append("RIFF");                                                       //file description header
+    header.append(pos - 8);                                                      //file size not including header
+    header.append("WAVE");                                                       //WAV description header
+    header.append("fmt ");                                                       //format description header
+    header.append(16);                                                           //size of WAVE section chunck
+    header.append(010);                                                          //PCM
+    header.append(this->format->getChannelsCount());                             //channels
+    header.append(this->format->getSampleRate());                                //samples per second
+    header.append(this->format->getBitrate());                                   //bytes per seconds
+    header.append(format->getChannelsCount() * format->getSampleSize() / 8);     //block aligmement
+    header.append(format->getSampleSize());                                      //bits per sample
+    header.append("data");                                                       //data description header
+    header.append(pos - header.size() - 4);                                      //size of data
+
+    this->file->seek(0);
+    this->file->write(header);
+    this->file->seek(pos);
 }
--- modules/filedevice.h
+++ modules/filedevice.h
@@ -9,15 +9,19 @@ class FileDevice : public ModuleDevice
 {
     //Q_OBJECT
 public:
-    FileDevice(QString filePath, QObject *parent);
+    FileDevice(QString filePath,AudioFormat *foramt, QObject *parent = 0);
     ~FileDevice();
-    static ModuleDevice *factory(QString name, AudioFormat *format, void *userData, QObject *parent);
+    static ModuleDevice *factory(QString name, AudioFormat *format, void *userData, QObject *parent = 0);
     qint64 readData(char *data, qint64 maxlen);
     qint64 writeData(const char *data, qint64 len);
     bool open(OpenMode mode);
 private:
     QFile       *file;
     void say(const QString message);
+    void writeWaveHeader();
+    AudioFormat *format;
+signals:
+    void debug(const QString message);
 };
 
 #endif // FILEDEVICE_H
